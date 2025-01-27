b24740c3e773b3db0ef78599be0e5d03e184bb9b
Allison Barry <87067546+aliibarry@users.noreply.github.com>
2025-01-27 20:06:53 +0100

    sex dimorphism across tissues

--- src/04-sex.R
+++ src/04-sex.R
@@ -21,17 +21,17 @@ names(background)[names(background) == "...1"] <- "Gene"
 
 head(enrichments)
 
-enrichments <- enrichments[enrichments$Tissue == "DRG"]
-df <- df[df$genes %in% enrichments$Gene, ]
+enrichment <- enrichments[enrichments$Tissue == "DRG",]
+df_sub <- df[df$genes %in% enrichment$Gene, ]
 
-head(df)
+head(df_sub)
 
 colData <-  colData[colData$Turbo  == "T", ]
 #colData <-  colData[colData$Tissue == "DRG", ]
 
 colData$Tissue.Sex <- as.factor(paste(colData$Tissue,colData$Sex, sep = "."))
 
-mat <- df %>% distinct(genes, .keep_all = TRUE) #6109 distinct genes
+mat <- df_sub %>% distinct(genes, .keep_all = TRUE) #6109 distinct genes
 rownames(mat) <- mat$genes
 
 mat$proteins <- NULL
@@ -97,6 +97,241 @@ dev.off()
 table(sig_de$logFC > 1)
 write.csv(results, paste(PATH_results, "DEP-analysis-limma-DRG.sex.csv"))
 write.csv(sig_de,  paste(PATH_results, "DEP-analysis-limma-DRG.sex_sig.csv"))
+
+#-------------------------------------------------------------------------------
+
+enrichment <- enrichments[enrichments$Tissue == "paw",]
+df_sub <- df[df$genes %in% enrichment$Gene, ]
+
+head(df_sub)
+
+colData <-  colData[colData$Turbo  == "T", ]
+#colData <-  colData[colData$Tissue == "DRG", ]
+
+colData$Tissue.Sex <- as.factor(paste(colData$Tissue,colData$Sex, sep = "."))
+
+mat <- df_sub %>% distinct(genes, .keep_all = TRUE) #6109 distinct genes
+rownames(mat) <- mat$genes
+
+mat$proteins <- NULL
+mat$genes    <- NULL
+
+mat <- mat[,colnames(mat) %in% colData$sampleID]
+
+index   <- match(colnames(mat), colData$sampleID)
+colData <- colData[index, ]
+
+head(colData)
+mat[1:5, 1:5]
+
+design <- model.matrix(~ 0 + Tissue.Sex, data = colData) #select design
+head(design)
+
+fit <- lmFit(mat, design)
+
+colnames(fit) #check possible comparisons
+
+contrast_matrix <- makeContrasts( Tissue.Sexpaw.m -  Tissue.Sexpaw.f, levels = design)
+fit2 <- contrasts.fit(fit, contrast_matrix)
+fit2 <- eBayes(fit2)
+
+results <- topTable(fit2, adjust="BH", number=Inf)
+
+head(results)
+
+sig_de <- results[results$adj.P.Val < 0.05 & abs(results$logFC) > 1, , drop = FALSE] #extract significant proteins if there
+head(sig_de)
+
+sig_de <- na.omit(sig_de)
+dim(sig_de)
+
+# volcano plotting
+res       <- as.data.frame(results) 
+mutateddf <- mutate(res, Sig=ifelse(res$adj.P.Val<0.05 & abs(res$logFC)>1, "FDR < 0.05 & LFC > 1", ifelse("NS")))
+input     <- cbind(gene=rownames(res), mutateddf) 
+
+volc = ggplot(input, aes(logFC, -log10(P.Value))) + geom_point(aes(col=Sig)) +
+  #scale_color_manual(values = c("#0D0887FF","#9512A1FF", "grey")) + 
+  scale_color_manual(values = c("#B63679ff", "grey")) + 
+  ggrepel::geom_text_repel(data=subset(input, input$gene %in% rownames(sig_de)),
+                           aes(label=gene), size=4, segment.alpha= 0.2, force =2, max.overlaps=16) 
+volc <- volc + theme_bw() + theme(aspect.ratio=1)
+volc <- volc + theme(legend.position="bottom", axis.text.y = element_text(size= 12, face="bold"), 
+                     axis.title.y = element_text(size=14), axis.title.x = element_text(size= 14), 
+                     axis.text.x = element_text(size= 12), legend.title=element_text(size=14), 
+                     legend.text=element_text(size=14), plot.title=element_text(size=12, hjust = 0.5)) + 
+  ggtitle("paw.sex")
+
+print(volc)
+
+pdf(file = paste0(PATH_results, "volcano-paw.sex.pdf"), height = 4, width = 4)
+print(volc)
+dev.off()
+
+pdf(file = paste0(PATH_results, "volcano-paw.sex_big.pdf"), height = 6, width = 6)
+print(volc)
+dev.off()
+
+table(sig_de$logFC > 1)
+write.csv(results, paste(PATH_results, "DEP-analysis-limma-paw.sex.csv"))
+write.csv(sig_de,  paste(PATH_results, "DEP-analysis-limma-paw.sex_sig.csv"))
+
+#-------------------------------------------------------------------------------
+
+enrichment <- enrichments[enrichments$Tissue == "SCN",]
+df_sub <- df[df$genes %in% enrichment$Gene, ]
+
+head(df_sub)
+
+colData <-  colData[colData$Turbo  == "T", ]
+#colData <-  colData[colData$Tissue == "DRG", ]
+
+colData$Tissue.Sex <- as.factor(paste(colData$Tissue,colData$Sex, sep = "."))
+
+mat <- df_sub %>% distinct(genes, .keep_all = TRUE) #6109 distinct genes
+rownames(mat) <- mat$genes
+
+mat$proteins <- NULL
+mat$genes    <- NULL
+
+mat <- mat[,colnames(mat) %in% colData$sampleID]
+
+index   <- match(colnames(mat), colData$sampleID)
+colData <- colData[index, ]
+
+head(colData)
+mat[1:5, 1:5]
+
+design <- model.matrix(~ 0 + Tissue.Sex, data = colData) #select design
+head(design)
+
+fit <- lmFit(mat, design)
+
+colnames(fit) #check possible comparisons
+
+contrast_matrix <- makeContrasts( Tissue.SexSCN.m -  Tissue.SexSCN.f, levels = design)
+fit2 <- contrasts.fit(fit, contrast_matrix)
+fit2 <- eBayes(fit2)
+
+results <- topTable(fit2, adjust="BH", number=Inf)
+
+head(results)
+
+sig_de <- results[results$adj.P.Val < 0.05 & abs(results$logFC) > 1, , drop = FALSE] #extract significant proteins if there
+head(sig_de)
+
+sig_de <- na.omit(sig_de)
+dim(sig_de)
+
+# volcano plotting
+res       <- as.data.frame(results) 
+mutateddf <- mutate(res, Sig=ifelse(res$adj.P.Val<0.05 & abs(res$logFC)>1, "FDR < 0.05 & LFC > 1", ifelse("NS")))
+input     <- cbind(gene=rownames(res), mutateddf) 
+
+volc = ggplot(input, aes(logFC, -log10(P.Value))) + geom_point(aes(col=Sig)) +
+  #scale_color_manual(values = c("#0D0887FF","#9512A1FF", "grey")) + 
+  scale_color_manual(values = c("#B63679ff", "grey")) + 
+  ggrepel::geom_text_repel(data=subset(input, input$gene %in% rownames(sig_de)),
+                           aes(label=gene), size=4, segment.alpha= 0.2, force =2, max.overlaps=16) 
+volc <- volc + theme_bw() + theme(aspect.ratio=1)
+volc <- volc + theme(legend.position="bottom", axis.text.y = element_text(size= 12, face="bold"), 
+                     axis.title.y = element_text(size=14), axis.title.x = element_text(size= 14), 
+                     axis.text.x = element_text(size= 12), legend.title=element_text(size=14), 
+                     legend.text=element_text(size=14), plot.title=element_text(size=12, hjust = 0.5)) + 
+  ggtitle("SCN.sex")
+
+print(volc)
+
+pdf(file = paste0(PATH_results, "volcano-SCN.sex.pdf"), height = 4, width = 4)
+print(volc)
+dev.off()
+
+pdf(file = paste0(PATH_results, "volcano-SCN.sex_big.pdf"), height = 6, width = 6)
+print(volc)
+dev.off()
+
+table(sig_de$logFC > 1)
+write.csv(results, paste(PATH_results, "DEP-analysis-limma-SCN.sex.csv"))
+write.csv(sig_de,  paste(PATH_results, "DEP-analysis-limma-SCN.sex_sig.csv"))
+
+#-------------------------------------------------------------------------------
+
+
+enrichment <- enrichments[enrichments$Tissue == "LSC",]
+df_sub <- df[df$genes %in% enrichment$Gene, ]
+
+head(df_sub)
+
+colData <-  colData[colData$Turbo  == "T", ]
+#colData <-  colData[colData$Tissue == "DRG", ]
+
+colData$Tissue.Sex <- as.factor(paste(colData$Tissue,colData$Sex, sep = "."))
+
+mat <- df_sub %>% distinct(genes, .keep_all = TRUE) #6109 distinct genes
+rownames(mat) <- mat$genes
+
+mat$proteins <- NULL
+mat$genes    <- NULL
+
+mat <- mat[,colnames(mat) %in% colData$sampleID]
+
+index   <- match(colnames(mat), colData$sampleID)
+colData <- colData[index, ]
+
+head(colData)
+mat[1:5, 1:5]
+
+design <- model.matrix(~ 0 + Tissue.Sex, data = colData) #select design
+head(design)
+
+fit <- lmFit(mat, design)
+
+colnames(fit) #check possible comparisons
+
+contrast_matrix <- makeContrasts( Tissue.SexLSC.m -  Tissue.SexLSC.f, levels = design)
+fit2 <- contrasts.fit(fit, contrast_matrix)
+fit2 <- eBayes(fit2)
+
+results <- topTable(fit2, adjust="BH", number=Inf)
+
+head(results)
+
+sig_de <- results[results$adj.P.Val < 0.05 & abs(results$logFC) > 1, , drop = FALSE] #extract significant proteins if there
+head(sig_de)
+
+sig_de <- na.omit(sig_de)
+dim(sig_de)
+
+# volcano plotting
+res       <- as.data.frame(results) 
+mutateddf <- mutate(res, Sig=ifelse(res$adj.P.Val<0.05 & abs(res$logFC)>1, "FDR < 0.05 & LFC > 1", ifelse("NS")))
+input     <- cbind(gene=rownames(res), mutateddf) 
+
+volc = ggplot(input, aes(logFC, -log10(P.Value))) + geom_point(aes(col=Sig)) +
+  #scale_color_manual(values = c("#0D0887FF","#9512A1FF", "grey")) + 
+  scale_color_manual(values = c("#B63679ff", "grey")) + 
+  ggrepel::geom_text_repel(data=subset(input, input$gene %in% rownames(sig_de)),
+                           aes(label=gene), size=4, segment.alpha= 0.2, force =2, max.overlaps=16) 
+volc <- volc + theme_bw() + theme(aspect.ratio=1)
+volc <- volc + theme(legend.position="bottom", axis.text.y = element_text(size= 12, face="bold"), 
+                     axis.title.y = element_text(size=14), axis.title.x = element_text(size= 14), 
+                     axis.text.x = element_text(size= 12), legend.title=element_text(size=14), 
+                     legend.text=element_text(size=14), plot.title=element_text(size=12, hjust = 0.5)) + 
+  ggtitle("LSC.sex")
+
+print(volc)
+
+pdf(file = paste0(PATH_results, "volcano-LSC.sex.pdf"), height = 4, width = 4)
+print(volc)
+dev.off()
+
+pdf(file = paste0(PATH_results, "volcano-LSC.sex_big.pdf"), height = 6, width = 6)
+print(volc)
+dev.off()
+
+table(sig_de$logFC > 1)
+write.csv(results, paste(PATH_results, "DEP-analysis-limma-LSC.sex.csv"))
+write.csv(sig_de,  paste(PATH_results, "DEP-analysis-limma-LSC.sex_sig.csv"))
 
 #-------------------------------------------------------------------------------
 
