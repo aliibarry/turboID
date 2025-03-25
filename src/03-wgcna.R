f6e1ee11658bd450a707a75d712b541eb59a9abe
Allison Barry <87067546+aliibarry@users.noreply.github.com>
2025-03-25 14:49:11 +0100

    refined wgcna filtering

--- src/03-wgcna.R
+++ src/03-wgcna.R
@@ -1,10 +1,13 @@
 library(dplyr)
 library(tidyr)
 library(ggplot2)
-library(WGCNA)
+library(viridis)
+library(ComplexHeatmap)
 
+library(WGCNA)
 library(clusterProfiler)
 library(enrichplot)
+library(org.Mm.eg.db)
 
 #dir.create("./output/WGCNA/")
 PATH_results = "./output/WGCNA/"
@@ -13,7 +16,6 @@ df          <- read.csv("./data/matrix-for-limma.csv", header = TRUE)
 colData     <- read.csv("./data/colData-for-limma.csv", header = TRUE)
 enrichments <- read.csv("./output/enrichments_75filt.csv", 
                         check.names = FALSE, header = TRUE, row.names = 1)
-
 #-------------------------------------------------------------------------------                                 
 
 # test_df <- enrichments$Gene[enrichments$Tissue == "paw"] 
@@ -21,58 +23,71 @@ test_df <- enrichments$Gene[!duplicated(enrichments$Gene)]
 
 head(test_df)
 
-df_paw <- df[df$genes %in% test_df, ]
-df_paw <- df_paw[!duplicated(df_paw$genes), ]
+df_filt <- df[df$genes %in% test_df, ]
+df_filt <- df_filt[!duplicated(df_filt$genes), ]
 
-rownames(df_paw) <- df_paw$genes
+rownames(df_filt) <- df_filt$genes
 
-df_paw$genes <- NULL
-df_paw$proteins <- NULL
+df_filt$genes    <- NULL
+df_filt$proteins <- NULL
 
-df_paw <- df_paw[colnames(df_paw) %in% colData$sampleID[colData$Turbo == "T"]]
-colData <- colData[match(colnames(df_paw), colData$sampleID), ]
+df_filt <- df_filt[colnames(df_filt) %in% colData$sampleID[colData$Turbo == "T"]]
+colData <- colData[match(colnames(df_filt), colData$sampleID), ]
+rownames(colData) <- colData$sampleID
 
-impute_median_per_group <- function(df, colData, group_col) {
-  # Ensure the group column exists in colData
-  if (!(group_col %in% colnames(colData))) {
-    stop(paste("Column", group_col, "does not exist in colData"))
-  }
-  
-  # Loop over each column of df
-  for (i in 1:ncol(df)) {
-    # If there are NAs in the column
-    if (any(is.na(df[, i]))) {
-      
-      # Get the corresponding group for each row in colData
-      group_ids <- colData[[group_col]]
-      
-      # Impute missing values for each group
-      for (group in unique(group_ids)) {
-        group_median <- median(df[group_ids == group, i], na.rm = TRUE)
-        df[group_ids == group & is.na(df[, i]), i] <- group_median
-      }
-    }
-  }
-  return(df)
+# #keep genes in more than one tissue only
+sample_tissue_map <- setNames(colData$Tissue, colData$sampleID)
+sample_tissues <- sample_tissue_map[colnames(df_filt)]
+
+filter_genes <- function(expression_values) {
+  expressed_tissues <- unique(na.omit(sample_tissues[!is.na(expression_values)]))
+  length(expressed_tissues) > 1
 }
 
-# impute missing values
-df_paw <- impute_median_per_group(df_paw, colData, "Tissue")
-table(is.na(df_paw))
+# Apply the filter function to rows (genes)
+df_filt <- df_filt[apply(df_filt, 1, filter_genes), ]
+ 
+# min_value    <- min(df_filt, na.rm = TRUE)
+# median_value <- median(as.matrix(df_filt), na.rm = TRUE)
+# 
+# impute_median_per_group <- function(df, colData, group_col) {
+# 
+#   colData   <- colData[colnames(df), , drop = FALSE]
+#   group_ids <- colData[[group_col]]
+# 
+#   # Apply function to each row (gene)
+#   df <- t(apply(df, 1, function(gene_expr) {
+# 
+#     group_medians <- tapply(gene_expr, group_ids, median, na.rm = TRUE)
+#     overall_median <- median(gene_expr, na.rm = TRUE)
+#     gene_expr[is.na(gene_expr)] <- ifelse(is.na(group_medians[group_ids[is.na(gene_expr)]]),
+#                                           overall_median, group_medians[group_ids[is.na(gene_expr)]])
+#     return(gene_expr)
+#   }))
+# 
+#   return(df)
+# }
+# 
+# df_filt <- impute_median_per_group(df_filt, colData, "Tissue")
+
+df_filt <- as.data.frame(df_filt)
+
+table(is.na(df_filt))
 
 #filter for more variable genes
-gene_variances <- apply(df_paw, 1, var, na.rm = TRUE)
+gene_variances <- apply(df_filt, 1, var, na.rm = TRUE)
 hist(gene_variances)
 range(gene_variances)
 
-df_paw <- df_paw[gene_variances >= 0.1, ]
+df_filt <- df_filt[gene_variances > 0, ]
+dim(df_filt)
 
 #-------------------------------------------------------------------------------
 
-options(stringsAsFactors = FALSE)
-enableWGCNAThreads()
+# options(stringsAsFactors = FALSE)
+# enableWGCNAThreads(nThreads = 8)
 
-datExpr <- as.data.frame(t(df_paw))
+datExpr <- as.data.frame(t(df_filt))
 
 powers <- c(1:50)  # Range of powers to test
 sft <- pickSoftThreshold(datExpr, powerVector = powers, verbose = 5, networkType = "signed")
@@ -82,12 +97,29 @@ plot(sft$fitIndices[,1], -sign(sft$fitIndices[,3])*sft$fitIndices[,2], type="o",
      main="Soft Threshold Selection")
 abline(h=0.8, col="red")  # Choose power where fit is ~0.9
 
-power <- 21  # Adjust based on previous step
-adjacency <- adjacency(datExpr, power = power)
+power <- 14  # Adjust based on previous step
+adjacency <- adjacency(datExpr, power = power, type = "signed")
 
 # Convert adjacency into a Topological Overlap Matrix (TOM)
-TOM <- TOMsimilarity(adjacency)
+TOM <- TOMsimilarity(adjacency, TOMType = "signed")
 dissTOM <- 1 - TOM
+
+#-------------------------------------------------------------------------------
+
+set.seed(52)
+selectedGenes <- sample(ncol(datExpr), 400)  
+TOMsubset <- TOM[selectedGenes, selectedGenes]
+
+# Visualize with a heatmap
+heatmap(TOMsubset, col = viridis::viridis(100), symm = TRUE)
+
+pheatmap(TOMsubset, 
+         color = viridis::viridis(100), 
+         clustering_method = "average", 
+         main = "TOM Heatmap (Subset of Genes)")
+
+#-------------------------------------------------------------------------------
+
 
 # Hierarchical clustering of genes
 geneTree <- hclust(as.dist(dissTOM), method = "average")
@@ -115,7 +147,7 @@ plotDendroAndColors(geneTree, moduleColors, "Dynamic Tree Cut",
 
 pdf(file = paste(PATH_results, "dendrogram_colours.pdf", sep=""), width = 8, height = 5)
 plotDendroAndColors(geneTree, moduleColors, "Dynamic Tree Cut",
-                    dendroLabels = FALSE, hang = 0.03, addGuide = TRUE, guideHang = 0.05)
+                    dendroLabels = FALSE, hang = 0.03, addGuide = FALSE, guideHang = 0.05)
 
 dev.off()
 
@@ -159,7 +191,7 @@ ordered_rows  <- order(tissue_values, decreasing = TRUE)
 moduleTraitCor_ordered <- moduleTraitCor[ordered_rows, , drop = FALSE]
 moduleTraitP_ordered    <- moduleTraitP[ordered_rows, , drop = FALSE]
 
-pdf(file = paste(PATH_results, "modules-heatmap.pdf", sep=""), width = 7, height = 9)
+pdf(file = paste(PATH_results, "modules-heatmap.pdf", sep=""), width = 6, height = 7)
 labeledHeatmap(Matrix = moduleTraitCor_ordered, 
                xLabels = colnames(traits),
                yLabels = rownames(moduleTraitCor_ordered), 
@@ -174,10 +206,8 @@ str(moduleTraitP)
 
 #---------------------------
 
-library(ComplexHeatmap)
-
-# Match the order of Tissue in traits
-tissue_names <- c("paw", "LSC", "DRG", "SCN") 
+traits
+tissue_names <- c("DRG","LSC", "paw", "SCN") #adjust order as needed, check names
 traits$Tissue_name <- tissue_names[traits$Tissue] 
 
 MEs_matrix <- t(MEs)  # Transpose MEs, so rows are modules and columns are samples
@@ -219,7 +249,7 @@ Heatmap(MEs_matrix_sig,
         heatmap_legend_param = list(title = "Eigengene Value")
 )
 
-pdf(file = paste(PATH_results, "modules-by-tissue-heatmap.pdf", sep=""), width = 8, height = 6)
+pdf(file = paste(PATH_results, "modules-by-tissue-heatmap.pdf", sep=""), width = 7, height = 6)
 Heatmap(MEs_matrix_sig, 
         name = "Module Eigengenes", 
         column_title = "Tissue",
@@ -248,18 +278,18 @@ MEs_long$Tissue <- factor(MEs_long$Tissue, levels = c("paw", "SCN", "DRG", "LSC"
 
 g <- ggplot(MEs_long, aes(x = Tissue, y = ME_value, fill = Tissue)) 
 g <- g + geom_boxplot() 
-g <- g + theme_bw() + facet_wrap(~ Module, scales = "free_y") 
+g <- g + theme_bw() + facet_wrap(~ Module, scales = "fixed") 
 g <- g +  # Facet by module, with independent y-scales
   theme(axis.text.x = element_text(angle = 45, hjust = 1)) +  # Rotate x-axis labels
   labs(x = " ", y = "ME Value", title = "MEs by Tissue and Module")
 
 print(g)
 
-pdf(file = paste(PATH_results, "modules-by-tissue.pdf", sep=""), width = 10, height = 8)
+pdf(file = paste(PATH_results, "modules-by-tissue.pdf", sep=""), width = 8, height = 7)
 print(g)
 dev.off()
 
-#---------------------------
+#-------------------------------------------------------------------------------
 
 module <- "green"
 moduleGenes <- colnames(datExpr)[moduleColors == module]
@@ -277,13 +307,14 @@ write.csv(MM, paste0(PATH_results,"module_green.csv"))
 
 #-------------------------------------------------------------------------------
 
-library(org.Mm.eg.db)
-
+# run GO analysis on each module
 background <- test_df
+# background <- rownames(df_filt)
 
 module <- "green"
 moduleGenes <- colnames(datExpr)[moduleColors == module]
 
+rm(ego2, ego)
 ego <- enrichGO(gene          = moduleGenes,
                 universe      = background,
                 OrgDb         = org.Mm.eg.db,
@@ -336,9 +367,51 @@ mutate(ego2, qscore = -log(p.adjust, base=10)) %>%
   barplot(x="qscore")
 dev.off()
 
-pdf(file = paste(PATH_results, "CC_green-upset.pdf", sep=""), width = 5, height = 4)
+pdf(file = paste(PATH_results, "CC_green-upset.pdf", sep=""), width = 7, height = 4)
 upsetplot(ego2)
 dev.off()
 
 write.csv(ego2, paste(PATH_results, "CC_green.csv"))
+
+#---
+
+ego <- enrichGO(gene          = moduleGenes,
+                universe      = background,
+                OrgDb         = org.Mm.eg.db,
+                keyType       = "SYMBOL",
+                ont           = "MF", 
+                pAdjustMethod = "BH",
+                pvalueCutoff  = 0.05,
+                qvalueCutoff  = 0.05,
+                readable      = TRUE)
+
+# # remove redundancy in the GO terms
+# ego2    <- clusterProfiler::simplify(ego, cutoff=0.7, by="p.adjust", select_fun=min, measure = 'Wang')
+
+pdf(file = paste(PATH_results, "MF_green-network.pdf", sep=""), width = 8, height = 8)
+goplot(ego)
+dev.off()
+
+pdf(file = paste(PATH_results, "MF_green-barplot.pdf", sep=""), width = 6, height = 3)
+mutate(ego, qscore = -log(p.adjust, base=10)) %>% 
+  barplot(x="qscore")
+dev.off()
+
+pdf(file = paste(PATH_results, "MF_green-upset.pdf", sep=""), width = 7, height = 4)
+upsetplot(ego)
+dev.off()
+
+write.csv(ego, paste(PATH_results, "MF_green.csv"))
+
+#---
+
+
+
+print(g)
+
+#-------------------------------------------------------------------------------
+
+#SCRATCH
+
+
 
